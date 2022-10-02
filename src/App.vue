ec6c0dcd1ba259344148c02722daa9cb4fca13d7
SummaryPuppet <adrianm_sv@hotmail.com>
2022-10-01 20:56:28 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import SpeechText from "./components/SpeechText.vue";
+</script>
+
+<template>
+  <section class="flex flex-col justify-center items-center gap-4">
+    <h1 class="text-5xl">Text speak</h1>
+    <SpeechText />
+  </section>
+</template>
---
+++ src/components/SpeechText.vue
@@ -0,0 +1,63 @@
+<script setup>
+import { onMounted, ref } from "vue";
+
+const speakBtnBool = ref(false);
+
+const selectVoice = ref("default");
+const textToSpeeach = ref("");
+
+const synthesis = window.speechSynthesis;
+let voices = ref([]);
+
+onMounted(() => {
+  voices.value = synthesis.getVoices();
+});
+
+function speakToText() {
+  if (!selectVoice) {
+    return alert("Select voice");
+  }
+
+  if (!textToSpeeach) {
+    return alert("Not void");
+  }
+  speakBtnBool.value = !speakBtnBool.value;
+
+  const utterThis = new SpeechSynthesisUtterance(textToSpeeach.value);
+
+  const nameSelectedVoice = selectVoice.value;
+  const voiceSelect = voices.value.find(
+    (voice) => voice.name == nameSelectedVoice
+  );
+
+  utterThis.voice = voiceSelect;
+
+  synthesis.speak(utterThis);
+}
+</script>
+
+<template>
+  <section class="flex flex-col gap-2">
+    <select v-model="selectVoice" class="bg-gray-100 p-1 rounded">
+      <option value="default">Voice default</option>
+      <option v-for="voice in voices" :value="voice.name">
+        {{ voice.name }}
+      </option>
+    </select>
+
+    <textarea
+      v-model="textToSpeeach"
+      cols="30"
+      rows="10"
+      placeholder="Insert text"
+      class="p-1 bg-gray-100 resize-none rounded"
+    ></textarea>
+
+    <button
+      @click="speakToText"
+      class="py-3 bg-gray-400 hover:bg-gray-600 rounded"
+    >
+      {{ speakBtnBool ? "Loading..." : "Speak" }}
+    </button>
+  </section>
+</template>
